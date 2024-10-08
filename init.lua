bf7293a763b1d7705d60a450caee45555615562c
Mohamed Heigab <>
2024-10-08 13:57:01 +0300

    some changes

--- init.lua
+++ init.lua
@@ -629,7 +629,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         eslint = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
